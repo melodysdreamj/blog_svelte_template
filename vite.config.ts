9ce7fd51beba41bd9342c5326dd1a2fc4e6069d1
june lee <melodysdreamj@gmail.com>
2024-11-02 15:22:41 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: 3000
+	},
 };
 
 export default config;
