cac83c7bbc952782b43936a06764d70d7ce06811
june lee <melodysdreamj@gmail.com>
2025-05-20 14:01:47 +0900

    Update _.ts

--- src/lib/server/worker_kv/_new/_.ts
+++ src/lib/server/worker_kv/_new/_.ts
@@ -1,112 +1,141 @@
 export class New {
+  constructor() {
+    this.docId = New.randomString(10);
+  }
 
-	constructor() {
-		this.docId = New.randomString(10);
-	}
+  // s000: string = "";
+  // i000: number = 0;
+  // b000: boolean = false;
+  // r000: number = 0.0;
+  // t000: Date = new Date(0);
+  // l000: string[] = [];
+  // c000: OtherModel = new OtherModel();
+  // j000 : OtherModel[] = [];
+  // e000: SomeEnum = SomeEnum.notSelected;
 
-	// s000: string = "";
-	// i000: number = 0;
-	// b000: boolean = false;
-	// r000: number = 0.0;
-	// t000: Date = new Date(0);
-	// l000: string[] = [];
-	// c000: OtherModel = new OtherModel();
-	// j000 : OtherModel[] = [];
-	// e000: SomeEnum = SomeEnum.notSelected;
+  docId: string = "";
 
-	docId: string = '';
+  toDataString(): string {
+    return btoa(
+      Array.from(
+        new TextEncoder().encode(
+          new URLSearchParams({
+            // s000: this.s000,
+            // i000: this.i000.toString(),
+            // b000: this.b000.toString(),
+            // r000: this.r000.toString(),
+            // t000: this.t000.getTime().toString(),
+            // l000: JSON.stringify(this.l000),
+            // c000: this.c000.toDataString(),
+            // j000: JSON.stringify(this.j000.map((model: OtherModel) => model.toDataString())),
+            // e000: this.e000,
+            docId: this.docId,
+          }).toString()
+        )
+      )
+        .map((byte) => String.fromCharCode(byte))
+        .join("")
+    );
+  }
 
-	toDataString(): string {
-		return btoa(Array.from(new TextEncoder().encode(new URLSearchParams({
-			// s000: this.s000,
-			// i000: this.i000.toString(),
-			// b000: this.b000.toString(),
-			// r000: this.r000.toString(),
-			// t000: this.t000.getTime().toString(),
-			// l000: JSON.stringify(this.l000),
-			// c000: this.c000.toDataString(),
-			// j000: JSON.stringify(this.j000.map((model: OtherModel) => model.toDataString())),
-			// e000: this.e000,
-			docId: this.docId
-		}).toString())).map(byte => String.fromCharCode(byte)).join(''));
-	}
+  static fromDataString(dataString: string): New {
+    const queryParams = Object.fromEntries(
+      new URLSearchParams(atob(dataString))
+    );
 
-	static fromDataString(dataString: string): New {
-		const queryParams = Object.fromEntries(new URLSearchParams(atob(dataString)));
+    const object = new New();
 
-		const object = new New();
+    // object.s000 = queryParams["s000"] || "";
+    // object.i000 = parseInt(queryParams["i000"] || "0", 10);
+    // object.b000 = queryParams["b000"] === "true";
+    // object.r000 = parseFloat(queryParams["r000"] || "0");
+    // object.t000 = new Date(parseInt(queryParams["t000"] || "0", 10));
+    // object.l000 = JSON.parse(queryParams["l000"] || "[]");
+    // object.c000 = OtherModel.fromDataString(queryParams["c000"] || new OtherModel().toDataString());
+    // object.j000 = (JSON.parse(queryParams["j000"] || "[]") || []).map((item: string) => OtherModel.fromDataString(item));
+    // object.e000 = SomeEnumHelper.fromString(queryParams["e000"] || SomeEnum.notSelected);
+    object.docId = queryParams["docId"] || "";
 
-		// object.s000 = queryParams["s000"] || "";
-		// object.i000 = parseInt(queryParams["i000"] || "0", 10);
-		// object.b000 = queryParams["b000"] === "true";
-		// object.r000 = parseFloat(queryParams["r000"] || "0");
-		// object.t000 = new Date(parseInt(queryParams["t000"] || "0", 10));
-		// object.l000 = JSON.parse(queryParams["l000"] || "[]");
-		// object.c000 = OtherModel.fromDataString(queryParams["c000"] || new OtherModel().toDataString());
-		// object.j000 = (JSON.parse(queryParams["j000"] || "[]") || []).map((item: string) => OtherModel.fromDataString(item));
-		// object.e000 = SomeEnumHelper.fromString(queryParams["e000"] || SomeEnum.notSelected);
-		object.docId = queryParams['docId'] || '';
+    return object;
+  }
 
-		return object;
-	}
+  toMap(): object {
+    return {
+      // s000: this.s000,
+      // i000: this.i000,
+      // b000: this.b000 ? 1 : 0,
+      // r000: this.r000,
+      // t000: this.t000.getTime(),
+      // l000: JSON.stringify(this.l000),
+      // c000: this.c000.toDataString(),
+      // j000: JSON.stringify(this.j000.map((model: OtherModel) => model.toDataString())),
+      // e000: this.e000,
+      docId: this.docId,
+    };
+  }
 
-	toMap(): object {
-		return {
-			// s000: this.s000,
-			// i000: this.i000,
-			// b000: this.b000 ? 1 : 0,
-			// r000: this.r000,
-			// t000: this.t000.getTime(),
-			// l000: JSON.stringify(this.l000),
-			// c000: this.c000.toDataString(),
-			// j000: JSON.stringify(this.j000.map((model: OtherModel) => model.toDataString())),
-			// e000: this.e000,
-			docId: this.docId
-		};
-	}
+  static fromMap(queryParams: any): New {
+    const object = new New();
 
-	static fromMap(queryParams: any): New {
-		const object = new New();
+    // object.s000 = queryParams.s000 || '';
+    // object.i000 = Number(queryParams.i000 || 0);
+    // object.b000 = queryParams.b000 === 1;
+    // object.r000 = queryParams.r000 || 0.0;
+    // object.t000 = new Date(queryParams.t000 || 0);
+    // object.l000 = JSON.parse(queryParams.l000 || '[]');
+    // object.c000 = OtherModel.fromDataString(queryParams.c000 || new OtherModel().toDataString());
+    // object.j000 = (JSON.parse(queryParams.j000 || '[]') || []).map((item: string) => OtherModel.fromDataString(item));
+    // object.e000 = SomeEnumHelper.fromString(queryParams.e000 || SomeEnum.notSelected);
+    object.docId = queryParams.docId;
 
-		// object.s000 = queryParams.s000 || '';
-		// object.i000 = Number(queryParams.i000 || 0);
-		// object.b000 = queryParams.b000 === 1;
-		// object.r000 = queryParams.r000 || 0.0;
-		// object.t000 = new Date(queryParams.t000 || 0);
-		// object.l000 = JSON.parse(queryParams.l000 || '[]');
-		// object.c000 = OtherModel.fromDataString(queryParams.c000 || new OtherModel().toDataString());
-		// object.j000 = (JSON.parse(queryParams.j000 || '[]') || []).map((item: string) => OtherModel.fromDataString(item));
-		// object.e000 = SomeEnumHelper.fromString(queryParams.e000 || SomeEnum.notSelected);
-		object.docId = queryParams.docId;
+    return object;
+  }
 
-		return object;
-	}
-
-	static randomString(length: number): string {
-		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		let result = '';
-		for (let i = 0; i < length; i++) {
-			result += characters.charAt(Math.floor(Math.random() * characters.length));
-		}
-		return result;
-	}
+  static randomString(length: number): string {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return result;
+  }
 }
-
 
 export class NewWorkerKV {
-	static async get(platform: any, docId: string): Promise<New | null> {
-		try {
-			const dataString = await platform.env.SentencePromptExample.get(docId);
+  static async get(platform: any, docId: string): Promise<New | null> {
+    try {
+      const dataString = await platform.env.SentencePromptExample.get(docId);
 
+      return New.fromDataString(dataString);
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  }
 
-			return New.fromDataString(dataString);
-		} catch (e) {
-			console.log(e);
-			return null;
-		}
-	}
+  static async upsert(platform: any, newData: New): Promise<boolean> {
+    try {
+      await platform.env.SentencePromptExample.put(
+        newData.docId,
+        newData.toDataString()
+      );
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  static async delete(platform: any, docId: string): Promise<boolean> {
+    try {
+      await platform.env.SentencePromptExample.delete(docId);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
 }
-
-
-
-
